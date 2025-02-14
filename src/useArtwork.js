2cd73c1facc097d1c694c08c8efae80fae9976a6
Marios Pls <35261805+MariosPliatsikas@users.noreply.github.com>
2025-02-14 11:30:50 +0100

    Create useArtwork.js

---
+++ src/useArtwork.js
@@ -0,0 +1,23 @@
+// src/useArtwork.js
+import { useEffect, useState } from 'react';
+import { fetchArtwork } from './api';
+
+export function useArtwork() {
+  const [artwork, setArtwork] = useState(null);
+
+  useEffect(() => {
+    async function getArtwork() {
+      const painting = await fetchArtwork();
+      setArtwork(painting);
+    }
+    getArtwork();
+
+    const interval = setInterval(() => {
+      getArtwork();
+    }, 600000); // 10 λεπτά σε χιλιοστά του δευτερολέπτου
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return artwork;
+}
