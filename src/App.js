2d55c2a4b57f758a187d168c43e3c1ac568d9b08
Marios Pls <35261805+MariosPliatsikas@users.noreply.github.com>
2025-02-14 12:24:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { fetchArtwork } from './api';
+import { useArtwork } from './useArtwork';
+import ArtworkInfo from './ArtworkInfo';
 import './App.css';
 
 function App() {
-  const [artwork, setArtwork] = useState(null);
+  const artwork = useArtwork();
   const [zoom, setZoom] = useState(1);
   const [backgroundPosition, setBackgroundPosition] = useState('center');
 
   useEffect(() => {
-    async function getArtwork() {
-      const painting = await fetchArtwork();
-      setArtwork(painting);
-    }
-    getArtwork();
-
     const interval = setInterval(() => {
-      getArtwork();
-    }, 600000); // 10 minutes in milliseconds
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const newZoom = Math.random() * 2 + 1; // Random zoom between 1 and 3
+      const newZoom = Math.random() * 2 + 1; // Τυχαίο zoom μεταξύ 1 και 3
       const newPositionX = Math.random() * 100;
       const newPositionY = Math.random() * 100;
       setZoom(newZoom);
       setBackgroundPosition(`${newPositionX}% ${newPositionY}%`);
-    }, 15000); // Change every 15 seconds
+    }, 15000); // Αλλαγή κάθε 15 δευτερόλεπτα
 
     return () => clearInterval(interval);
   }, []);
@@ -39,11 +26,7 @@ function App() {
 
   return (
     <div className="App" style={{ backgroundImage: `url(${artwork.primaryImage})`, backgroundSize: `${zoom * 100}%`, backgroundPosition }}>
-      <div className="description">
-        <h1>{artwork.title}</h1>
-        <p>{artwork.artistDisplayName}</p>
-        <p>{artwork.objectDate}</p>
-      </div>
+      <ArtworkInfo artwork={artwork} />
     </div>
   );
 }
