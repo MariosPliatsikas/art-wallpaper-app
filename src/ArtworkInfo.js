4f8f6e5cf1da8a2c5c76cd05277ca19a9983158d
Marios Pls <35261805+MariosPliatsikas@users.noreply.github.com>
2025-02-15 18:47:54 +0100

    Update ArtworkInfo.js

--- src/ArtworkInfo.js
+++ src/ArtworkInfo.js
@@ -3,9 +3,7 @@ import React from 'react';
 function ArtworkInfo({ artwork }) {
   return (
     <div className="description">
-      <h1>{artwork.title}</h1>
       <p>{artwork.artistDisplayName}</p>
-      <p>{artwork.objectDate}</p>
     </div>
   );
 }
