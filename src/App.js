217429828b8f69dbef196c0471d03e9fcddc38fd
Marios Pls <35261805+MariosPliatsikas@users.noreply.github.com>
2025-02-16 08:44:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,37 +2,49 @@ import React, { useEffect, useState } from 'react';
 import { useArtwork } from './useArtwork';
 import ArtworkInfo from './ArtworkInfo';
 import './App.css';
+import { useHistory } from 'react-router-dom'; // Προσθέστε την βιβλιοθήκη για ανακατεύθυνση
 
 function App() {
   const artwork = useArtwork();
   const [backgroundPositionX, setBackgroundPositionX] = useState('50%');
-  const [intervalTime, setIntervalTime] = useState(15000); // default interval time
+  const [intervalTime, setIntervalTime] = useState(30000); // Αυξήστε το διάστημα για λιγότερες ενημερώσεις
   const [hideText, setHideText] = useState(false);
+  const history = useHistory(); // Δημιουργία ιστορικού για ανακατεύθυνση
 
   useEffect(() => {
     const interval = setInterval(() => {
       const newPositionX = Math.random() * 100;
       setBackgroundPositionX(`${newPositionX}%`);
-    }, intervalTime); // Χρήση του προσαρμοσμένου διαστήματος
+    }, intervalTime);
 
     return () => clearInterval(interval);
   }, [intervalTime]);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      setHideText(true);
-    }, 15000); // Απόκρυψη μετά από 15 δευτερόλεπτα
+    if (!hideText) {
+      const timeout = setTimeout(() => {
+        setHideText(true);
+      }, 15000); // Απόκρυψη μετά από 15 δευτερόλεπτα
 
-    return () => clearTimeout(timeout);
-  }, []);
+      return () => clearTimeout(timeout);
+    }
+  }, [hideText]);
+
+  useEffect(() => {
+    if (!artwork) {
+      history.push('/next-page'); // Ανακατεύθυνση στην επόμενη σελίδα αν δεν υπάρχει έργο
+    }
+  }, [artwork, history]);
 
   if (!artwork) {
     return <div>Loading...</div>;
   }
 
+  const MemoizedArtworkInfo = React.memo(ArtworkInfo);
+
   return (
     <div className="App" style={{ backgroundImage: `url(${artwork.primaryImage})`, backgroundPosition: `${backgroundPositionX} center`, backgroundSize: 'contain' }}>
-      {!hideText && <ArtworkInfo artwork={artwork} />}
+      {!hideText && <MemoizedArtworkInfo artwork={artwork} />}
       <div className="bottom-right">
         <h1>{artwork.title}</h1>
         <p>{artwork.objectDate}</p>
