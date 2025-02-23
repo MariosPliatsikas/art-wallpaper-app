7547c533dde4e330f714a8b95856e9b8de880b0e
MarPls <pliatsikasmarios@gmail.com>
2025-02-23 16:27:18 +0100

    config-overrides.j

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,21 @@
+
 const path = require('path');
 
 module.exports = function override(config, env) {
-  // Βρείτε τον κανόνα για το source-map-loader και εξαιρέστε όλα τα node modules
-  config.module.rules.forEach(rule => {
-    if (rule.enforce === 'pre' && rule.use && rule.use.some(loader => loader.loader === 
-'source-map-loader')) {
-      rule.exclude = /node_modules/;
-    }
-  });
+  // Βρείτε τον κανόνα για το source-map-loader
+  const sourceMapRule = config.module.rules.find(
+    (rule) =>
+      rule.enforce === 'pre' &&
+      rule.use &&
+      rule.use.some((loader) => loader.loader === 'source-map-loader')
+  );
+
+  // Αν βρεθεί ο κανόνας, εξαιρέστε τα node_modules
+  if (sourceMapRule) {
+    sourceMapRule.exclude = /node_modules/;
+  } else {
+    console.warn('Source map loader rule not found!');
+  }
 
   return config;
 };
