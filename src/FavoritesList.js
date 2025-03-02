ed9a670807f47315d6c1fb915003b241f0e8bd62
MarPls <pliatsikasmarios@gmail.com>
2025-03-02 12:21:30 +0100

    Προσθήκη λειτουργίας αγαπημένων, εμφάνιση λίστας αγαπημένων και κουμπί εκκαθάρισης

--- src/FavoritesList.js
+++ src/FavoritesList.js
@@ -1,13 +1,14 @@
 
-import React from 'react';
+iimport React from 'react';
 
-const FavoritesList = ({ favorites }) => {
+const FavoritesList = ({ favorites, onSelectFavorite, onClearFavorites }) => {
   return (
     <div className="favorites-list">
       <h2>Favorites</h2>
+      <button className="clear-favorites-button" onClick={onClearFavorites}>Clear Favorites</button>
       <ul>
         {favorites.map((item, index) => (
-          <li key={index}>
+          <li key={index} onClick={() => onSelectFavorite(item)}>
             <img src={item.primaryImage} alt={item.title} />
             <p>{item.title}</p>
           </li>
