b5c5b4cf0d0347614fb10e3d323f9e03a54b6b52
MarPls <pliatsikasmarios@gmail.com>
2025-03-04 20:20:31 +0100

    ...

---
+++ src/RijksmuseumPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function RijksmuseumPage() {
+  return (
+    <div>
+      <h1>Rijksmuseum Artworks</h1>
+      <p>Εδώ θα εμφανίζονται τα έργα τέχνης από το Rijksmuseum.</p>
+    </div>
+  );
+}
+
+export default RijksmuseumPage;
+
