c2a113bbf80a9af5442307b7d9f34d31a7cc5f27
MarPls <pliatsikasmarios@gmail.com>
2025-03-30 12:59:02 +0200

    01

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_caching import Cache
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
 
 db = SQLAlchemy()
 cache = Cache()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -21,6 +23,9 @@ def create_app():
 
     # Initialize SQLAlchemy
     db.init_app(app)
+
+    # Initialize Flask-Migrate
+    migrate.init_app(app, db)
 
     # Initialize Cache
     cache.init_app(app)
