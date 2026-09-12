e13e9a4a5e640a002a4d3dea8d6dab2fd500ac68
Marios Pls <35261805+MariosPliatsikas@users.noreply.github.com>
2026-09-12 20:36:35 +0200

    cleanup: remove obsolete NMA configuration

--- src/config.js
+++ src/config.js
@@ -5,12 +5,7 @@
 const config = {
   MET_MUSEUM_API_URL: 'https://collectionapi.metmuseum.org/public/collection/v1.1',
   HARVARD_API_URL: 'https://api.harvardartmuseums.org',
-  NMA_API_URL: 'https://data.nma.gov.au',
-
   HARVARD_API_KEY: process.env.REACT_APP_HARVARD_API_KEY || '',
-  NMA_API_KEY: process.env.REACT_APP_NMA_API_KEY || '',
-
-  DEFAULT_QUERY: 'painting',
 };
 
 export default config;
